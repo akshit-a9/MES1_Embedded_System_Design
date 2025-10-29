b97f71201508c3d081432ba41a34321fa6367068
akshit-a9 <akshitsharma2002@gmail.com>
2025-10-29 22:45:34 +0530

    Midsem Question 1 Added

--- Midsem_Questions/q1_branching.s
+++ Midsem_Questions/q1_branching.s
@@ -4,14 +4,11 @@ IN      DCD     0x20000100, Reset_Handler
         ENTRY
 
 Reset_Handler
-        ; -------------------------
-        ; Initialize registers (except R4)
-        ; -------------------------
+
         MOV     r0,  #0
         MOV     r1,  #1
         MOV     r2,  #2
         MOV     r3,  #3
-        ; r4 used for loading data
         MOV     r5,  #5
         MOV     r6,  #6
         MOV     r7,  #7
@@ -21,9 +18,6 @@ Reset_Handler
         MOV     r11, #11
         MOV     r12, #12
 
-        ; -------------------------
-        ; Load and compare using R4
-        ; -------------------------
         LDR     r4, =d1
         LDR     r4, [r4]
         CMP     r4, #0
@@ -31,24 +25,22 @@ Reset_Handler
         BGT     is_pos
 
 is_neg
-        PUSH    {r9-r12}        ; if negative
+        PUSH    {r9-r12}        
         B       done
 
 is_pos
-        PUSH    {r5-r8}         ; if positive
+        PUSH    {r5-r8}         
         B       done
 
 is_zero
-        PUSH    {r0-r3}         ; if zero
+        PUSH    {r0-r3}         
 
 done
 hang    B       hang
 
-        ; -------------------------
-        ; Data section
-        ; -------------------------
+
         AREA    DATA, DATA, READWRITE
-d1      DCD     0               ; change value for testing
+d1      DCD     37              
 
         END
 
